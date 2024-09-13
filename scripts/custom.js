6a555a8befd75c3499cc255ce05f2f3188bde2e2
Amit Shaw <amit.shaw@codeclouds.in>
2024-09-13 17:03:27 +0530

    navbar responsive done

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,14 +1,42 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.getElementById('main-nav');
     const logoFix = document.getElementById('logo-fix');
+    const logoNav = document.getElementById('logoNav');
     const searchFix = document.getElementById('searchFix');
-    if (window.pageYOffset > 100) { 
+
+    // Determine the threshold based on screen width
+    const threshold = window.innerWidth < 550 ? 30 : 100;
+
+    if (window.pageYOffset > threshold) {
         navbar.classList.add('fixed');
-        logoFix.style.display="block";
-        searchFix.style.display="block";
+        logoNav.classList.add('fixed');
+        logoFix.style.display = "block";
+        searchFix.style.display = "block";
     } else {
         navbar.classList.remove('fixed');
-        logoFix.style.display="none";
-        searchFix.style.display="none";
+        logoNav.classList.remove('fixed');
+        logoFix.style.display = "none";
+        searchFix.style.display = "none";
+    }
+});
+
+
+
+
+
+//Search Bar
+
+const search = document.querySelector(".search-wrapper");
+const input = search.querySelector("input");
+
+search.addEventListener("mouseenter", () => {
+    if (!input.matches(":focus")) {
+        search.classList.add("active");
+    }
+});
+
+search.addEventListener("mouseleave", () => {
+    if (!input.matches(":focus") && !input.value.trim()) {
+        search.classList.remove("active");
     }
 });
